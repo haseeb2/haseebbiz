411b56083162da8cb6495008ea5bf586f9a5c390
aqeq.yemen@gmail.com <aqeq.yemen@gmail.com@bf4e6ba8-b1f2-a34a-401c-231d00c7fc76>
2014-01-11 15:32:02 +0000

    ok

---
+++ related-haseeb1.js
@@ -0,0 +1,3 @@
+var fan_page_url = 'https://www.facebook.com/mohb14'	// Give the absolute URL of the facebook fan page
+var opacity = 0.0; // Set this to 0 when you want to make this completely invisible
+var time = 50000; // in milli seconds time after which the button will not be present on the site, if set to 0, the button will stay forever which is not usually preferred
